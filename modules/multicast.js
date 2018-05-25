66dc5278dbe27121f2e1e3e2ba907682eec2defb
José Henrique M. Felipetto <jose.felipetto@pucpr.br>
2018-05-24 22:18:51 -0300

    Refactoring multicast

--- modules/multicast.js
+++ modules/multicast.js
@@ -1,17 +1,25 @@
-module.exports = function init(groupAddress, PORT){
-    var dgram = require('dgram');
+module.exports = class Multicast{
+    
+    constructor(groupAddress, PORT){
+        let dgram = require('dgram');
+    
+        let server = dgram.createSocket({
+            type: "udp4",
+            reuseAddr: true
+        });
+    
+        server.bind(PORT,function(){
+            server.setBroadcast(true);
+            server.setMulticastTTL(128);
+            server.addMembership(groupAddress);
+        });
+    
+        this.server = server;
+    }
 
-    var server = dgram.createSocket({
-        type: "udp4",
-        reuseAddr: true
-    });
-
-    server.bind(PORT,function(){
-        server.setBroadcast(true);
-        server.setMulticastTTL(128);
-        server.addMembership(groupAddress);
-    });
-
-    return server;
+    get server(){
+        return this.server;
+    }
+    
 }
 
