97386bf7834f93ee3d74a5bfa0b8da2a74f091ba
Laboratório Novas Tecnologias <labcwb@MacBook-Pro-de-Laboratorio.local>
2018-06-01 10:48:29 -0300

    Event Emitter working

--- tests/process2.js
+++ tests/process2.js
@@ -5,3 +5,7 @@ let multicastSocket = new Multicast('224.0.0.1',42280);
 setInterval(function(){
     multicastSocket.send("Eu sou o beta");
 },2000);
+
+multicastSocket.on('message', (msg) => {
+    console.log('\nRecebi: ', msg)
+})
